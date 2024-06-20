a723d584e9e9b36a592bffcac62ceadb50a63682
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-06-20 12:13:59 -0600

    Configuración inicial de la arquitectura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MiniMapComponent } from './maps/components/mini-map/mini-map.component';
+import { SideMenuComponent } from './maps/components/side-menu/side-menu.component';
+import { MapsLayoutComponent } from './maps/layout/maps-layout/maps-layout.component';
+import { FullScreenPageComponent } from './maps/pages/full-screen-page/full-screen-page.component';
+import { MarkersPageComponent } from './maps/pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './maps/pages/properties-page/properties-page.component';
+import { ZoomRangePageComponent } from './maps/pages/zoom-range-page/zoom-range-page.component';
+/* import { MapsModule } from './maps/maps/maps.module'; */
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiniMapComponent,
+    SideMenuComponent,
+    MapsLayoutComponent,
+    FullScreenPageComponent,
+    MarkersPageComponent,
+    PropertiesPageComponent,
+    ZoomRangePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // MapsModule : Lo vamos a querer cargar con LazyLoad
   ],
   providers: [],
   bootstrap: [AppComponent]
